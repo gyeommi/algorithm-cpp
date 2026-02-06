3da12e1702587bccae20d906b606c9f524b6b8bd
gyeommi <kkm417417@naver.com>
2026-02-06 13:34:43 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s, p;
+    int t, r;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> r >> s;
+        for (int j = 0; j < s.length(); j++)
+        {
+            for (int k = 0; k < r; k++)
+                cout << s[j];
+        }
+        cout << "\n";
+    }
+    return 0;
+}

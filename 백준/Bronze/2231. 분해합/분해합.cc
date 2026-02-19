0f483b7ff2dc4c4cb6b5cb8023df0ddc45e6bd7e
gyeommi <kkm417417@naver.com>
2026-02-19 15:27:11 +0900

    [Bronze II] Title: 분해합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, result = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int sum = 0;
+		int num = i;
+		while (num != 0)
+		{
+			sum += num % 10;
+			num /= 10;
+		}
+		if (sum + i == n)
+		{
+			result = i;
+			break;
+		}
+	}
+
+	cout << result << endl;
+
+	return 0;
+}

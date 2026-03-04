42fbd011d6f600d7a8530c44b682e633ba4863f1
gyeommi <kkm417417@naver.com>
2026-03-04 11:45:11 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    
+    for (char ch : my_string) {
+        if (ch >= '0' && ch <= '9')
+            answer += (ch - '0');
+    }
+    return answer;
+}

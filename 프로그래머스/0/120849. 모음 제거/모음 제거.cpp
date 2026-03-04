b89df72c94c8a13b269f4849fbdbc7ec24abae7f
gyeommi <kkm417417@naver.com>
2026-03-04 11:37:56 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for (char ch : my_string) {
+        if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+            answer += ch;
+        }
+    }
+    
+    return answer;
+}

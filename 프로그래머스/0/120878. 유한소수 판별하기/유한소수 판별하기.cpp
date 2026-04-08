02e719b7a6cb2a7bbecf2c56cab3f9a6857fe346
gyeommi <kkm417417@naver.com>
2026-04-08 14:22:24 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int common = std::gcd(a, b);
+    b /= common;
+
+    while (b % 2 == 0) b /= 2;
+    while (b % 5 == 0) b /= 5;
+
+    return (b == 1) ? 1 : 2;
+}

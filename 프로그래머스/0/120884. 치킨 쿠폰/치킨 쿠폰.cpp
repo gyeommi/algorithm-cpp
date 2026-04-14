fab4f2d594ea3b04d8b4071b5b3e059e84e1ee3e
gyeommi <kkm417417@naver.com>
2026-04-14 15:25:44 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int chicken) {
+    int answer = 0;
+    int coupon = chicken;
+
+    while (coupon >= 10) {
+        int service = coupon / 10;
+        answer += service;
+        coupon = service + coupon % 10;
+    }
+
+    return answer;
+}
